3a0b9160b4f6f46029c1d6bc90a2fe768b818612
caixueyuan <2529982859@qq.com>
2017-06-13 11:53:15 +0800

    finish the problem

--- iss001.java
+++ iss001.java
@@ -1,5 +1,6 @@
 public class iss001{
 	public static void main(String[] args){
-		System.out.println("this issure001 has been finished");
+		System.out.println("I had finished this problem");
+		System.out.println("finish the hotfix and i can do this now");
 	}
 }
